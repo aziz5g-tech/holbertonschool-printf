14d52081cf2e70d4acc599fb46d709c6303e535e
noneals <124405266+noneals@users.noreply.github.com>
2025-09-28 21:34:53 +0300

    Create print_functions.c

---
+++ print_functions.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_char - prints a character
+ * @args: argument list
+ * Return: number of characters printed
+ */
+int print_char(va_list args)
+{
+    char c = va_arg(args, int);
+    _putchar(c);
+    return (1);
+}
+
+/**
+ * print_string - prints a string
+ * @args: argument list
+ * Return: number of characters printed
+ */
+int print_string(va_list args)
+{
+    char *s = va_arg(args, char *);
+    int i = 0;
+
+    if (s == NULL)
+        s = "(null)";
+
+    while (s[i])
+    {
+        _putchar(s[i]);
+        i++;
+    }
+    return (i);
+}
+
+/**
+ * print_percent - prints a percent sign
+ * @args: argument list (unused)
+ * Return: number of characters printed
+ */
+int print_percent(va_list args)
+{
+    (void)args;
+    _putchar('%');
+    return (1);
+}
