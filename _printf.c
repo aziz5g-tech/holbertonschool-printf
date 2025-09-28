fc6d99085bbbef9b7e092b62b1ae738c6a33d0bb
root <root@ip-10-42-71-92.ec2.internal>
2025-09-28 19:31:34 +0000

    updates

--- _printf.c
+++ _printf.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * handle_specifier - handles format specifiers
+ * @c: the format specifier character
+ * @args: the argument list
+ *
+ * Return: The number of printed characters for the specifier
+ */
+static int handle_specifier(char c, va_list args)
+{
+    if (c == 'c')
+        return print_char(args);
+    else if (c == 's')
+        return print_string(args);
+    else if (c == '%')
+        return print_percent(args);
+
+    _putchar('%');
+    _putchar(c);
+    return 2;
+}
+
+/**
  * _printf - prints a formatted string
  * @format: the string to format
  *
@@ -8,46 +29,31 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i;
-	int count = 0;
+    va_list args;
+    int i, count = 0;
 
-	if (format == NULL)
-		return (-1);
+    if (!format)
+        return -1;
 
-	va_start(args, format);
-
-	for (i = 0; format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (!format[i + 1]) /* case: single '%' */
-			{
-				va_end(args);
-				return (-1);
-			}
-
-			i++;
-			if (format[i] == 'c')
-				count += print_char(args);
-			else if (format[i] == 's')
-				count += print_string(args);
-			else if (format[i] == '%')
-				count += print_percent(args);
-			else
-			{
-				_putchar('%');
-				_putchar(format[i]);
-				count += 2;
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			count++;
-		}
-	}
-
-	va_end(args);
-	return (count);
+    va_start(args, format);
+    for (i = 0; format[i]; i++)
+    {
+        if (format[i] == '%')
+        {
+            if (!format[i + 1])
+            {
+                va_end(args);
+                return -1;
+            }
+            i++;
+            count += handle_specifier(format[i], args);
+        }
+        else
+        {
+            _putchar(format[i]);
+            count++;
+        }
+    }
+    va_end(args);
+    return count;
 }
