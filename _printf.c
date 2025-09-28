96c3ef46906aa1f7a331bd4ed34eaea805406f8c
noneals <124405266+noneals@users.noreply.github.com>
2025-09-28 21:41:53 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -8,46 +8,46 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i;
-    int count = 0;
+	va_list args;
+	int i;
+	int count = 0;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (i = 0; format[i]; i++)
-    {
-        if (format[i] == '%')
-        {
-            if (!format[i + 1]) /* حالة % لحالها */
-            {
-                va_end(args);
-                return (-1);
-            }
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (!format[i + 1]) /* case: single '%' */
+			{
+				va_end(args);
+				return (-1);
+			}
 
-            i++;
-            if (format[i] == 'c')
-                count += print_char(args);
-            else if (format[i] == 's')
-                count += print_string(args);
-            else if (format[i] == '%')
-                count += print_percent(args);
-            else
-            {
-                _putchar('%');
-                _putchar(format[i]);
-                count += 2;
-            }
-        }
-        else
-        {
-            _putchar(format[i]);
-            count++;
-        }
-    }
+			i++;
+			if (format[i] == 'c')
+				count += print_char(args);
+			else if (format[i] == 's')
+				count += print_string(args);
+			else if (format[i] == '%')
+				count += print_percent(args);
+			else
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				count += 2;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+	}
 
-    va_end(args);
-    return (count);
+	va_end(args);
+	return (count);
 }
