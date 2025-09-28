e4d8a620351a77d4cae0392e76201e4c3e01779b
root <root@ip-10-42-71-92.ec2.internal>
2025-09-28 19:42:16 +0000

    updates

--- _printf.c
+++ _printf.c
@@ -9,16 +9,15 @@
  */
 static int handle_specifier(char c, va_list args)
 {
-    if (c == 'c')
-        return print_char(args);
-    else if (c == 's')
-        return print_string(args);
-    else if (c == '%')
-        return print_percent(args);
-
-    _putchar('%');
-    _putchar(c);
-    return 2;
+if (c == 'c')
+return print_char(args);
+else if (c == 's')
+return print_string(args);
+else if (c == '%')
+return print_percent(args);
+_putchar('%');
+_putchar(c);
+return 2;
 }
 
 /**
@@ -29,31 +28,29 @@ static int handle_specifier(char c, va_list args)
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i, count = 0;
-
-    if (!format)
-        return -1;
-
-    va_start(args, format);
-    for (i = 0; format[i]; i++)
-    {
-        if (format[i] == '%')
-        {
-            if (!format[i + 1])
-            {
-                va_end(args);
-                return -1;
-            }
-            i++;
-            count += handle_specifier(format[i], args);
-        }
-        else
-        {
-            _putchar(format[i]);
-            count++;
-        }
-    }
-    va_end(args);
-    return count;
+va_list args;
+int i, count = 0;
+if (!format)
+return -1;
+va_start(args, format);
+for (i = 0; format[i]; i++)
+{
+if (format[i] == '%')
+{
+if (!format[i + 1])
+{
+va_end(args);
+return -1;
+}
+i++;
+count += handle_specifier(format[i], args);
+}
+else
+{
+_putchar(format[i]);
+count++;
+}
+}
+va_end(args);
+return (count);
 }
