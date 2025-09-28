27cf150d6b7879eadc1e0191b408dd94d78beb2f
noneals <124405266+noneals@users.noreply.github.com>
2025-09-28 21:37:04 +0300

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,10 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned.
+ * Return: On success 1, on error -1
  */
 int _putchar(char c)
 {
