2a2275ed1fbc99ea9099ec7f2eb05dc96450c66f
root <root@ip-10-42-50-255.ec2.internal>
2025-09-30 12:42:12 +0000

    fix: function for max and min in print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -48,32 +48,46 @@ int print_percent(va_list args)
 }
 
 /**
+ * print_number - helper function to print a positive number
+ * @num: the number to print
+ * Return: number of characters printed
+ */
+static int print_number(unsigned int num)
+{
+	int count = 0;
+
+	if (num / 10)
+		count += print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+	count++;
+
+	return (count);
+}
+
+/**
  * print_integer - prints an integer
  * @args: argument list
  * Return: number of characters printed
  */
 int print_integer(va_list args)
 {
-int n = va_arg(args, int);
- int count = 0;
- unsigned int num;
+	int n = va_arg(args, int);
+	int count = 0;
+	unsigned int num;
 
- if (n < 0)
- {
-  _putchar('-');
-  count++;
-  num = -n;
- }
- else
- {
-  num = n;
- }
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		num = (unsigned int)(-(n + 1)) + 1;
+	}
+	else
+	{
+		num = n;
+	}
 
- if (num / 10)
-  count += print_integer((va_list)(unsigned int[]){num / 10});
+	count += print_number(num);
 
- _putchar((num % 10) + '0');
- count++;
-
- return (count);	
+	return (count);
 }
