4c99dc95159381c9a21d817c9a08fdc4293850ed
root <root@ip-10-42-114-213.ec2.internal>
2025-10-01 21:12:43 +0000

    style: change format code and remove extra code not uses

--- _printf.c
+++ _printf.c
@@ -1,58 +1,50 @@
 #include "main.h"
 
 /**
- * print_binary_number - prints a binary number
- * @n: the unsigned long number to print
- *
- * Return: The number of printed characters
- */
-static int print_binary_number(unsigned long n)
-{
-	int count = 0;
-
-	if (n > 1)
-		count += print_binary_number(n / 2);
-
-	_putchar((n % 2) + '0');
-	return (count + 1);
-}
-
-/**
- * handle_specifier - handles format specifiers
+ * handle_specifier_buffer - handles format specifiers with buffer
  * @c: the format specifier character
  * @args: the argument list
+ * @buffer: the buffer to use
+ * @index: pointer to buffer index
  *
  * Return: The number of printed characters for the specifier
-  *
- * Supported specifiers: c, s, %, d, i, b, u, o, x, X
  */
-static int handle_specifier(char c, va_list args)
+static int handle_specifier_buffer(char c, va_list args, char *buffer, int *index)
 {
-    if (c == 'c')
-        return (print_char(args));
-    else if (c == 's')
-        return (print_string(args));
-    else if (c == '%')
-        return (print_percent(args));
-    else if (c == 'd' || c == 'i')
-        return (print_integer(args));
-    else if (c == 'b')
-    {
-        unsigned int n = va_arg(args, unsigned int);
-        return (print_binary_number(n));
-    }
-    else if (c == 'u')
-        return (print_unsigned(args));
-    else if (c == 'o')
-        return (print_octal(args));
-    else if (c == 'x')
-        return (print_hex_lower(args));
-    else if (c == 'X')
-        return (print_hex_upper(args));
+	if (c == 'c')
+		return (print_char_buffer(args, buffer, index));
+	else if (c == 's')
+		return (print_string_buffer(args, buffer, index));
+	else if (c == '%')
+		return (print_percent_buffer(args, buffer, index));
+	else if (c == 'd' || c == 'i')
+		return (print_integer_buffer(args, buffer, index));
+	else if (c == 'b')
+	{
+		unsigned int n = va_arg(args, unsigned int);
+		return (print_binary_buffer(n, buffer, index));
+	}
+	else if (c == 'u')
+		return (print_unsigned_buffer(args, buffer, index));
+	else if (c == 'o')
+	{
+		unsigned int n = va_arg(args, unsigned int);
+		return (print_octal_buffer(n, buffer, index));
+	}
+	else if (c == 'x')
+	{
+		unsigned int n = va_arg(args, unsigned int);
+		return (print_hex_buffer(n, buffer, index, 0));
+	}
+	else if (c == 'X')
+	{
+		unsigned int n = va_arg(args, unsigned int);
+		return (print_hex_buffer(n, buffer, index, 1));
+	}
 
-    _putchar('%');
-    _putchar(c);
-    return (2);
+	add_to_buffer('%', buffer, index);
+	add_to_buffer(c, buffer, index);
+	return (2);
 }
 
 /**
@@ -65,6 +57,8 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int i, count = 0;
+	char buffer[BUFFER_SIZE];
+	int buffer_index = 0;
 
 	if (!format)
 		return (-1);
@@ -77,19 +71,21 @@ int _printf(const char *format, ...)
 		{
 			if (!format[i + 1])
 			{
+				flush_buffer(buffer, &buffer_index);
 				va_end(args);
 				return (-1);
 			}
 			i++;
-			count += handle_specifier(format[i], args);
+			count += handle_specifier_buffer(format[i], args, buffer, &buffer_index);
 		}
 		else
 		{
-			_putchar(format[i]);
+			add_to_buffer(format[i], buffer, &buffer_index);
 			count++;
 		}
 	}
 
+	flush_buffer(buffer, &buffer_index);
 	va_end(args);
 	return (count);
 }
