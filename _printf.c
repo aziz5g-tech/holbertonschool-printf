2ff11a680abfee74859b547b33b15950200375de
lina_aziz <lenaexo0@gmail.com>
2025-10-01 21:35:54 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -26,24 +26,31 @@ static int print_binary_number(unsigned long n)
  */
 static int handle_specifier(char c, va_list args)
 {
-	if (c == 'c')
-		return (print_char(args));
-	else if (c == 's')
-		return (print_string(args));
-	else if (c == '%')
-		return (print_percent(args));
-	else if (c == 'd' || c == 'i')
-		return (print_integer(args));
-	else if (c == 'b')
-	{
-		unsigned int n = va_arg(args, unsigned int);
+    if (c == 'c')
+        return (print_char(args));
+    else if (c == 's')
+        return (print_string(args));
+    else if (c == '%')
+        return (print_percent(args));
+    else if (c == 'd' || c == 'i')
+        return (print_integer(args));
+    else if (c == 'b')
+    {
+        unsigned int n = va_arg(args, unsigned int);
+        return (print_binary_number(n));
+    }
+    else if (c == 'u')
+        return (print_unsigned(args));
+    else if (c == 'o')
+        return (print_octal(args));
+    else if (c == 'x')
+        return (print_hex_lower(args));
+    else if (c == 'X')
+        return (print_hex_upper(args));
 
-		return (print_binary_number(n));
-	}
-
-	_putchar('%');
-	_putchar(c);
-	return (2);
+    _putchar('%');
+    _putchar(c);
+    return (2);
 }
 
 /**
