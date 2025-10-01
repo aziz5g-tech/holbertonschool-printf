480cd488f5d0d3f82aee820e0808168ef7cd285b
noneals <124405266+noneals@users.noreply.github.com>
2025-10-01 17:49:10 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -61,3 +61,39 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (count);
 }
+/**
+ * print_binary - prints an unsigned int in binary
+ * @args: argument list containing the unsigned int
+ *
+ * Return: number of characters printed
+ */
+int print_binary(va_list args)
+{
+    unsigned int n = va_arg(args, unsigned int);
+    unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
+    int started = 0, count = 0;
+
+    if (n == 0)
+    {
+        _putchar('0');
+        return (1);
+    }
+
+    while (mask)
+    {
+        if (n & mask)
+        {
+            _putchar('1');
+            count++;
+            started = 1;
+        }
+        else if (started)
+        {
+            _putchar('0');
+            count++;
+        }
+        mask >>= 1;
+    }
+
+    return (count);
+}
