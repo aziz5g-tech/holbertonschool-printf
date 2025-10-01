a5c52033f17e1fb2953bdcab38490f1787ce72db
root <root@ip-10-42-114-213.ec2.internal>
2025-10-01 20:24:56 +0000

    feat: new file (buffer_utils.c)

---
+++ buffer_utils.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * flush_buffer - writes buffer content to stdout and resets buffer
+ * @buffer: the buffer to flush
+ * @index: pointer to buffer index
+ *
+ * Return: number of characters written
+ */
+int flush_buffer(char *buffer, int *index)
+{
+    int written = 0;
+
+    if (*index > 0)
+    {
+        written = write(1, buffer, *index);
+        *index = 0;
+    }
+    return (written);
+}
+
+/**
+ * add_to_buffer - adds a character to buffer
+ * @c: character to add
+ * @buffer: the buffer to add to
+ * @index: pointer to buffer index
+ *
+ * Return: 1 on success
+ */
+int add_to_buffer(char c, char *buffer, int *index)
+{
+    if (*index >= BUFFER_SIZE)
+        flush_buffer(buffer, index);
+
+    buffer[(*index)++] = c;
+    return (1);
+}
