f62ea62614041d6d44787bc247da813498711d63
root <root@ip-10-42-114-213.ec2.internal>
2025-10-01 20:27:51 +0000

    feat: add prototypes in main.h to use buffer utility functions

--- main.h
+++ main.h
@@ -3,17 +3,29 @@
 
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <limits.h>
 
+#define BUFFER_SIZE 1024
+
+/* Core printf function */
 int _printf(const char *format, ...);
-int _putchar(char c);
+
+/* Print functions */
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
 int print_integer(va_list args);
-int print_binary(va_list args);
-int print_unsigned(va_list args);     
-int print_octal(va_list args);       
-int print_hex_lower(va_list args); 
-int print_hex_upper(va_list args);   
+int print_unsigned(va_list args);
+int print_octal(va_list args);
+int print_hex_lower(va_list args);
+int print_hex_upper(va_list args);
+
+/* Buffer utility functions */
+int flush_buffer(char *buffer, int *index);
+int add_to_buffer(char c, char *buffer, int *index);
+
+/* Helper functions */
+int _putchar(char c);
+
 #endif /* MAIN_H */
