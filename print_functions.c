9ec48de7925cc2d6fe2c79dbeec18be890cab764
lina_aziz <lenaexo0@gmail.com>
2025-10-01 21:45:17 +0300

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -168,3 +168,30 @@ int print_hex_lower(va_list args)
 
     return (count);
 }
+
+/**
+ * print_hex_upper - prints an unsigned integer in uppercase hex
+ * @args: argument list
+ *
+ * Return: number of characters printed
+ */
+int print_hex_upper(va_list args)
+{
+    unsigned int n = va_arg(args, unsigned int);
+    char buf[50];
+    char *hex = "0123456789ABCDEF";
+    int i = 0, count = 0;
+
+    if (n == 0)
+        return (_putchar('0'));
+
+    while (n)
+    {
+        buf[i++] = hex[n % 16];
+        n /= 16;
+    }
+    while (i--)
+        count += _putchar(buf[i]);
+
+    return (count);
+}
