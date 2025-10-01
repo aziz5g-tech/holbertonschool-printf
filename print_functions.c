f72b2b11d1c0c0d87cfbb5d74163a3135c6d0aee
lina_aziz <lenaexo0@gmail.com>
2025-10-01 21:44:50 +0300

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -142,3 +142,29 @@ int print_octal(va_list args)
 
     return (count);
 }
+/**
+ * print_hex_lower - prints an unsigned integer in lowercase hex
+ * @args: argument list
+ *
+ * Return: number of characters printed
+ */
+int print_hex_lower(va_list args)
+{
+    unsigned int n = va_arg(args, unsigned int);
+    char buf[50];
+    char *hex = "0123456789abcdef";
+    int i = 0, count = 0;
+
+    if (n == 0)
+        return (_putchar('0'));
+
+    while (n)
+    {
+        buf[i++] = hex[n % 16];
+        n /= 16;
+    }
+    while (i--)
+        count += _putchar(buf[i]);
+
+    return (count);
+}
