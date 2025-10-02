e5f07ad2b942b10f74e4a3f997a8638102dc856b
root <root@ip-10-42-223-86.ec2.internal>
2025-10-02 14:41:53 +0000

    fix: betty-style for  buffer_print_function.c

--- buffer_print_functions.c
+++ buffer_print_functions.c
@@ -183,9 +183,17 @@ int print_hex_buffer(unsigned int num, char *buffer, int *index, int uppercase)
     return (count + 1);
 }
 
+/**
+ * print_string_special_buffer - prints a string with non-printable chars as \xXX
+ * @args: argument list containing the string
+ * @buffer: the buffer to use
+ * @index: pointer to buffer index
+ *
+ * Return: number of characters printed
+ */
 int print_string_special_buffer(va_list args, char *buffer, int *index)
 {
-char *str = va_arg(args, char *);
+    char *str = va_arg(args, char *);
     int count = 0;
     int i;
 
@@ -194,7 +202,7 @@ char *str = va_arg(args, char *);
 
     for (i = 0; str[i]; i++)
     {
-      if (str[i] > 0 && (str[i] < 32 || str[i] >= 127))
+        if (str[i] > 0 && (str[i] < 32 || str[i] >= 127))
         {
             /* Print non-printable character as \xXX */
             add_to_buffer('\\', buffer, index);
