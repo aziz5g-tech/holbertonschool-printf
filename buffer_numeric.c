81ec77ef9f7a87f8ae0f6d913307c44b995ef368
lina_aziz <lenaexo0@gmail.com>
2025-10-04 09:32:54 +0300

    Update buffer_numeric.c

--- buffer_numeric.c
+++ buffer_numeric.c
@@ -1,120 +1,129 @@
 #include "main.h"
 
-/* Global bitmask for flags of the current '%' sequence. */
-int g_flags = 0;
-
 /**
- * handle_specifier_buffer - handles format specifiers with buffer
- * @c: the format specifier character
- * @args: the argument list
+ * print_number_buffer - helper function to print number digits
+ * @num: number to print
  * @buffer: the buffer to use
  * @index: pointer to buffer index
  *
- * Return: number of printed characters for the specifier
+ * Return: number of characters printed
  */
-static int handle_specifier_buffer(char c, va_list args,
-				   char *buffer, int *index)
+int print_number_buffer(unsigned int num, char *buffer, int *index)
 {
-	if (c == 'c')
-		return (print_char_buffer(args, buffer, index));
-	else if (c == 's')
-		return (print_string_buffer(args, buffer, index));
-	else if (c == 'S')
-		return (print_string_special_buffer(args, buffer, index));
-	else if (c == '%')
-		return (print_percent_buffer(args, buffer, index));
-	else if (c == 'd' || c == 'i')
-		return (print_integer_buffer(args, buffer, index));
-	else if (c == 'b')
-	{
-		unsigned int n = va_arg(args, unsigned int);
+	int count = 0;
 
-		return (print_binary_buffer(n, buffer, index));
-	}
-	else if (c == 'u')
-		return (print_unsigned_buffer(args, buffer, index));
-	else if (c == 'o')
-	{
-		unsigned int n = va_arg(args, unsigned int);
+	if (num / 10)
+		count += print_number_buffer(num / 10, buffer, index);
 
-		return (print_octal_buffer(n, buffer, index));
-	}
-	else if (c == 'x')
-	{
-		unsigned int n = va_arg(args, unsigned int);
-
-		return (print_hex_buffer(n, buffer, index, 0));
-	}
-	else if (c == 'X')
-	{
-		unsigned int n = va_arg(args, unsigned int);
-
-		return (print_hex_buffer(n, buffer, index, 1));
-	}
-
-	/* Unknown specifier: print it verbatim. */
-	add_to_buffer('%', buffer, index);
-	add_to_buffer(c, buffer, index);
-	return (2);
+	add_to_buffer((num % 10) + '0', buffer, index);
+	return (count + 1);
 }
 
 /**
- * _printf - prints a formatted string
- * @format: the string to format
+ * print_integer_buffer - prints an integer using buffer
+ * @args: argument list containing the integer
+ * @buffer: the buffer to use
+ * @index: pointer to buffer index
  *
- * Return: number of printed characters, or -1 if format is NULL
+ * Return: number of characters printed
  */
-int _printf(const char *format, ...)
+int print_integer_buffer(va_list args, char *buffer, int *index)
 {
-	va_list args;
-	int i, count = 0;
-	char buffer[BUFFER_SIZE];
-	int buffer_index = 0;
+	int n = va_arg(args, int);
+	unsigned int num;
+	int count = 0;
 
-	if (!format)
-		return (-1);
-
-	va_start(args, format);
-
-	for (i = 0; format[i]; i++)
+	if (n < 0)
 	{
-		if (format[i] == '%')
-		{
-			/* End of string with a lone '%' -> error. */
-			if (!format[i + 1])
-			{
-				flush_buffer(buffer, &buffer_index);
-				va_end(args);
-				return (-1);
-			}
+		/* negative sign for signed decimals */
+		add_to_buffer('-', buffer, index);
+		count++;
+		/* convert to magnitude without UB on INT_MIN */
+		num = (unsigned int)(-(n + 1)) + 1;
+	}
+	else
+	{
+		num = (unsigned int)n;
 
-			i++; /* We saw '%' and advanced. */
-
-			/* Parse flags (+, space, #) after '%' and before the specifier. */
-			g_flags = 0;
-			while (format[i] == '+' || format[i] == ' ' || format[i] == '#')
-			{
-				if (format[i] == '+')
-					g_flags |= FLAG_PLUS;
-				else if (format[i] == ' ')
-					g_flags |= FLAG_SPACE;
-				else /* '#' */
-					g_flags |= FLAG_HASH;
-				i++;
-			}
-
-			/* Delegate printing with flags available via g_flags. */
-			count += handle_specifier_buffer(format[i], args,
-							 buffer, &buffer_index);
-		}
-		else
-		{
-			add_to_buffer(format[i], buffer, &buffer_index);
-			count++;
-		}
+		/* Print '+' or a leading space for positive values when flags request it. */
+		if (g_flags & FLAG_PLUS)
+			count += add_to_buffer('+', buffer, index);
+		else if (g_flags & FLAG_SPACE)
+			count += add_to_buffer(' ', buffer, index);
 	}
 
-	flush_buffer(buffer, &buffer_index);
-	va_end(args);
+	count += print_number_buffer(num, buffer, index);
 	return (count);
+}
+
+/**
+ * print_unsigned_buffer - prints an unsigned integer using buffer
+ * @args: argument list containing the unsigned integer
+ * @buffer: the buffer to use
+ * @index: pointer to buffer index
+ *
+ * Return: number of characters printed
+ */
+int print_unsigned_buffer(va_list args, char *buffer, int *index)
+{
+	unsigned int num = va_arg(args, unsigned int);
+
+	/* No sign for %u even if '+' or ' ' flags are present. */
+	return (print_number_buffer(num, buffer, index));
+}
+
+/**
+ * print_octal_buffer - prints an octal number using buffer
+ * @num: number to print
+ * @buffer: the buffer to use
+ * @index: pointer to buffer index
+ *
+ * Return: number of characters printed
+ */
+int print_octal_buffer(unsigned int num, char *buffer, int *index)
+{
+	int count = 0;
+
+	/* With '#', emit a single leading '0' for non-zero octal values (once). */
+	if ((num != 0) && (g_flags & FLAG_HASH) && (num / 8 == 0))
+		count += add_to_buffer('0', buffer, index);
+
+	if (num / 8)
+		count += print_octal_buffer(num / 8, buffer, index);
+
+	add_to_buffer((num % 8) + '0', buffer, index);
+	return (count + 1);
+}
+
+/**
+ * print_hex_buffer - prints a hexadecimal number using buffer
+ * @num: number to print
+ * @buffer: the buffer to use
+ * @index: pointer to buffer index
+ * @uppercase: 1 for uppercase, 0 for lowercase
+ *
+ * Return: number of characters printed
+ */
+int print_hex_buffer(unsigned int num, char *buffer, int *index, int uppercase)
+{
+	int count = 0;
+	char hex_char;
+
+	/* With '#', emit '0x'/'0X' for non-zero hex values (printed once). */
+	if ((num != 0) && (g_flags & FLAG_HASH) && (num / 16 == 0))
+	{
+		count += add_to_buffer('0', buffer, index);
+		count += add_to_buffer(uppercase ? 'X' : 'x', buffer, index);
+	}
+
+	if (num / 16)
+		count += print_hex_buffer(num / 16, buffer, index, uppercase);
+
+	if ((num % 16) < 10)
+		hex_char = (num % 16) + '0';
+	else
+		hex_char = (num % 16) - 10 + (uppercase ? 'A' : 'a');
+
+	add_to_buffer(hex_char, buffer, index);
+	return (count + 1);
 }
